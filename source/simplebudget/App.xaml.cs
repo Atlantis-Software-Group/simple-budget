f9243e5cdb2a68e8cb3805471e301bf1896fb2cf
Neil Shah <SwapnilShah99@gmail.com>
2023-03-04 14:00:43 -0500

    initial commit of SimpleBudget maui app

---
+++ source/simplebudget/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace simplebudget
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
