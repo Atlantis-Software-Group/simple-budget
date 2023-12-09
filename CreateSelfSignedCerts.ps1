3e6df39a75c4237bf8e4016c4f780b16de299463
ShahSwapnil <SwapnilShah99@gmail.com>
2023-12-09 16:26:44 -0500

    organize things

--- CreateSelfSignedCerts.ps1
+++
@@ -1,5 +0,0 @@
-$certname = "simple-budget1"    ## Replace {certificateName}
-$cert = New-SelfSignedCertificate -Subject "CN=$certname" -CertStoreLocation "Cert:\CurrentUser\My" -KeyExportPolicy Exportable -KeySpec Signature -KeyLength 2048 -KeyAlgorithm RSA -HashAlgorithm SHA256
-
-$mypwd = ConvertTo-SecureString -String "test" -Force -AsPlainText  ## Replace {myPassword}
-Export-PfxCertificate -Cert $cert -FilePath ".\certs\$certname.pfx" -Password $mypwd   ## Specify your preferred location
--- README.md
+++ README.md
@@ -19,7 +19,7 @@ docker run --rm -it -p externalport:internalport <imagename>:<tag>
 
 ## Docker compose command
 ```
-docker compose  -f "docker-compose.yml" up -d identity 
+docker-compose -f "simple-budget.yml" up -d identity 
 ```
 # SQL Server
 
