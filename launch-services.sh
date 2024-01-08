733fd44063d7b92cedd301166c551049b7ebca26
ShahSwapnil <SwapnilShah99@gmail.com>
2024-01-07 19:45:05 -0500

    add launch-services.sh
    launch docker compose services
    launch web browser

---
+++ launch-services.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+MainFunction() 
+{
+    cd ..
+    StartDockerServices
+    StartFirefox
+}
+
+StartDockerServices() {
+    echo -e "${YELLOW}Starting Docker Services ${NC}"
+    cd simple-budget
+    docker-compose up -d --wait
+}
+
+StartFirefox() {
+    echo -e "${YELLOW}Starting Web browser ${NC}"
+    #start firefox https://localhost:3100 https://localhost:3101/swagger https://localhost:3102/swagger https://localhost:3103
+    start firefox https://localhost:3101/swagger https://localhost:3102/swagger https://localhost:3103 https://localhost:3104
+}
+
+MainFunction
